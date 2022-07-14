8a06134ed978a6f7d76108c04f22614123e65b13
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-07-14 16:30:08 +0200

    2-6 ok

---
+++ 2-6.js
@@ -0,0 +1,24 @@
+let user = prompt("Enter a number between 1 and 7")
+let monday = "Monday"
+let tuesday = "Tuesday"
+let wednesday = "Wednesday"
+let thursday = "Thursday"
+let friday = "Friday"
+let saturday = "Saturday"
+let sunday = "Sunday"
+
+if (user == 1){
+    console.log(monday)
+} else if (user == 2){
+    console.log(tuesday)
+} else if (user == 3){
+    console.log(wednesday)
+} else if (user == 4){
+    console.log(thursday)
+} else if (user == 5){
+    console.log(friday)
+} else if (user == 6){
+    console.log(saturday)
+} else if (user == 7){
+    console.log(sunday)
+}
