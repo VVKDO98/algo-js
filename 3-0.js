d498a252199257d415d69f569fe810bb52ac022f
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-07-15 09:47:43 +0200

    3-0 ok

---
+++ 3-0.js
@@ -0,0 +1,25 @@
+let arrayWilson = ["Baudson Guillaume",
+"Benaccetta Rinaldo",
+"Blaevoet Benoit",
+"Brigode Xavier",
+"Charles Xavier",
+"Corda Daniela",
+"Denis Anthony",
+"Devilers Vincent",
+"Elinckx Julien",
+"Friquet Lucas",
+"Gillard Sophie",
+"Kirac Axel",
+"Maddouri Hazem",
+"Maurcot Dylan",
+"Moraes Kamilla",
+"Pécher Stéphanie",
+"Scourneau Julien",
+"Vanvolcksom Doriano",
+"Vervoot Eddy",
+"Elyahyioui Selim",
+"Moussiaux Titouan"]
+
+for (let elem of arrayWilson){
+    console.log("Hello " + elem)
+}
