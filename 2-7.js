5c0c3a5387342cbae00c5f4559af2ee8f0a7e135
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-07-15 09:27:00 +0200

    2-7 ok

--- 2-7.js
+++ 2-7.js
@@ -1,5 +1,8 @@
+let n = prompt("Enter a number between 1 and 5")
+let result = 0
 
-
-for (let n = prompt("Enter a number between 1 and 5"); n === n; n++){
-    
+for (let i = 0; i < n; i++){
+    result += Number(prompt("Enter a number"));
 }
+
+console.log(result)
