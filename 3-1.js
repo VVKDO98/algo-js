91387aed6136b546d89396a60ff23a67e3cd4845
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-07-15 10:08:26 +0200

    3-1 ok

---
+++ 3-1.js
@@ -0,0 +1,17 @@
+let arrnum = [1, 2, 3, 4, 5]
+let arrhigh = [100, 101, 102]
+let sum = 0
+let sumhigh = 0
+
+for(let i = 0; i < arrnum.length; i++){
+    sum += arrnum[i];
+} console.log(sum)
+
+for(let i = 0; i < arrhigh.length; i++){
+    sumhigh += arrhigh[i]
+} console.log(sumhigh)
+
+
+
+
+
