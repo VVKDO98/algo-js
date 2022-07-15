27959e1dd9fb291d68ffa41f82884fb09bd046ed
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-07-15 10:30:20 +0200

    3-2 ok

---
+++ 3-2.js
@@ -0,0 +1,19 @@
+let average = 0
+let arrAv = [1, 2, 3, 4, 5]
+
+for(let i = 0; i < arrAv.length; i++){
+    average += arrAv[i];
+}
+
+console.log(average / arrAv.length)
+
+// 
+
+average = 0
+let arrHi = [100, 101, 102]
+
+for(let i = 0; i < arrHi.length; i++){
+    average += arrHi[i];
+}
+
+console.log(average / arrHi.length)
